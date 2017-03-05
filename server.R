361c278951c6287706461b530f7473fa93056829
Ben Celsi <Ben Celsi>
2017-03-04 19:59:08 -0800

    finish random button

--- server.R
+++ server.R
@@ -3,14 +3,15 @@ source("finalproject.R")
 
 server<- function(input,output){
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  observeEvent(input$generate, {
+    print(input$country2)
+    rate <- cleaned.data[[paste0(input$gender, input$year)]][min(which(countries == input$country2))]
+    output$result <- renderPrint(rate)
+    random <- runif(1, 1, 1000)
+    if (random > rate) {
+      output$result <- renderPrint("Yes, you have survived past the age of five.")
+    } else {
+      output$result <- renderPrint("No, you have died before the age of five.")
+    }
+  })
 }
--- ui.R
+++ ui.R
@@ -6,10 +6,19 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       selectInput("country","Country", choices=countries)
-    )
-  )
+    ),
+    mainPanel()
+  ),
+  
+  #Random part  
+  
+  
+  selectInput("gender","Gender", choices=list("male", "female")),
+  selectInput("country2","Country", choices=countries),
+  selectInput("year","Year", choices=list(1990,2000,2010,2015)),
+  actionButton("generate", "Will you live past the age of five?"),
+  textOutput("result")
+ 
 )
-
-
 
 shinyApp(ui,server)
